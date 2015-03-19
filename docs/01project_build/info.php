4fa38cfc3a67b0cb47cec36696475ff0c928057b
taohs <50584211@qq.com>
2015-03-19 11:03:57 +0800

    新建文件夹

---
+++ docs/01project_build/info.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ${FILENAME} class file.
+ * This file contains classes implementing attribute collection feature.
+ *
+ * @author TaoHaiSong <taohaisong@gmail.com>
+ * @date: 15/3/19  Time: 上午11:03
+ * @link http://www.php4s.com/
+ * @copyright 
+ * @license http://www.php4s.com/license/
+ * @package PHP
+ */
